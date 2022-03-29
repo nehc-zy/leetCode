350f434aec5eb697480d5388fd67e9db930597d4
GitOfNelsonChen <1063356646@qq.com>
2022-03-29 19:57:45 +0800

    计组笔记，leetcode下一个排列数 2022.3.29

--- leetcode/leetCode.cpp
+++ leetcode/leetCode.cpp
@@ -9,10 +9,8 @@ void Solution::quickSort(std::vector<int>& nums, int left, int right) {
 	while (low < high) {
 		while (low < high && nums[high] >= temp)high--;
 		nums[low] = nums[high];
-		if (low < high)low++;
 		while (low < high && nums[low] < temp)low++;
 		nums[high] = nums[low];
-		if (low < high)high--;
 	}
 	nums[low] = temp;
 	quickSort(nums, left, low - 1);
@@ -20,15 +18,53 @@ void Solution::quickSort(std::vector<int>& nums, int left, int right) {
 	return;
 }
 
+void Solution::quickSort2(std::vector<int>& nums, int left, int right) {
+	if (left >= right)return;
+	int temp = nums[left];
+	int low = left;
+	int high = right;
+	while (low < high) {
+		while (low < high && nums[high] >= temp)high--;
+		//这个小于等于能保证low指向的位置在low==high都是小于或者等于基准的值
+		//假设之前有交换，high逐渐减少，碰到low，所以是小值，或者high找到一个小值，low向右，碰到high，指向小值
+		//假设之前没交换，high直接到low，low指向的值等于基准值
+		//等于是为了防止基准值到后面去
+		while (low < high && nums[low] <= temp)low++;
+		//不用判断low小于high
+		std::swap(nums[low], nums[high]);
+		
+	}
+	std::swap(nums[left], nums[low]);
+	quickSort2(nums, left, low - 1);
+	quickSort2(nums, low + 1, right);
+	return;
+}
+
+
+void Solution::nextPermutation(std::vector<int>& nums) {
+	for (int i = nums.size() - 1; i >= 0; i--) {
+		for (int j = i - 1; j >= 0; j--) {
+			if (nums[i] > nums[j]) {
+				std::swap(nums[i], nums[j]);
+				std::reverse(nums.begin() + i, nums.end());
+				return;
+			}
+		}
+	}
+	std::reverse(nums.begin(), nums.end());
+	return;
+}
 
 int main() 
 {
-	//std::vector<int> nums = { -1,0,1,2,-1,-4 };
-	std::vector<int> nums = { 0,0,0,0,0};
-	Solution::quickSort(nums, 0, nums.size() - 1);
-	for (int i = 0; i < nums.size() - 1; i++) {
+	std::vector<int> nums = {2,3,1};
+	//std::vector<int> nums = { 0,0,0,0,0};
+	//Solution::quickSort2(nums, 0, nums.size() - 1);
+	Solution::nextPermutation(nums);
+	for (int i = 0; i < nums.size(); i++) {
 		std::cout << nums[i] << std::endl;
 	}
+	system("pause");
 	return 0;
 
 }
--- leetcode/solution.h
+++ leetcode/solution.h
@@ -3,5 +3,9 @@
 class Solution 
 {
 public:
+	//quickSort实现1
 	static void quickSort(std::vector<int>& nums, int left, int right);
+	//quickSort实现2
+	static void quickSort2(std::vector<int>&nums, int left, int right);
+	static void nextPermutation(std::vector<int>& nums);
 };
